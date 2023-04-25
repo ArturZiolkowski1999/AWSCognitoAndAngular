7579e35397e66702dfcf17056031f757caa9b1c6
Artur Ziółkowski <aaartiii99@gmail.com>
2023-04-25 08:58:32 +0200

    register interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { JwtInterceptor } from './jwt-interceptor.service';
 
 
 
@@ -28,6 +32,11 @@ import { SignUpComponent } from './auth/sign-up/sign-up.component';
     
   ],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [
     AppComponent,
