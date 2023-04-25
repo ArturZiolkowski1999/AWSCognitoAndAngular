6e08de813437197b5b725d104a4d6ed581912706
ArturZiolkowski1999 <63076661+ArturZiolkowski1999@users.noreply.github.com>
2023-04-25 09:02:41 +0200

    style

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    cognito: {
-        userPoolId: 'eu-north-1_B1B7YHp86',
-        userPoolWebClientId: 'jjmee56dbp7c3gjhbnrr9jv35',
-    }
-};
