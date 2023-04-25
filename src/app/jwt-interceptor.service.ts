78584bf0d435cfa07afe69d7cbfbf1945132e1a3
Artur Ziółkowski <aaartiii99@gmail.com>
2023-04-25 09:22:25 +0200

    idToken interceptor

--- src/app/jwt-interceptor.service.ts
+++ src/app/jwt-interceptor.service.ts
@@ -8,26 +8,20 @@ import { Auth } from 'aws-amplify';
 export class JwtInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Tutaj możesz pobrać token JWT i przypisać go do zmiennej
-    const token = 'TWÓJ_TOKEN_JWT'; // Zastąp tym kodem, który pobierze Twój token JWT
-
-    // Dodaj token JWT do nagłówka Authorization żądania HTTP
-    if (token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-    }
 
     return from(Auth.currentSession())
             .pipe(
                 switchMap((auth: any) => { // switchMap() is used instead of map().
 
                     let jwt = auth.accessToken.jwtToken;
+                    let idToken = auth.idToken.jwtToken;
                     let with_auth_request = request.clone({
                         setHeaders: {
                             Authorization: `Bearer ${jwt}`
+                        },
+                        body: {
+                          ...request.body,
+                          idToken: idToken
                         }
                     });
                     console.log("Cloned",with_auth_request);
